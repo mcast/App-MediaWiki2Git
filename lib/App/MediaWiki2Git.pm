64646c1075ae3353a2dc2854d49a2d454c08bf27
Matthew Astley <beeep$mca@t8o.org>
2011-10-11 22:28:44 +0100

    POD overhaul.
    and derived README.md update

--- lib/App/MediaWiki2Git.pm
+++ lib/App/MediaWiki2Git.pm
@@ -14,30 +14,88 @@ use Carp;
 
 App::MediaWiki2Git - copy MediaWiki page history into a Git repository
 
+=head1 SYNOPSIS
+
+ # Set up
+ mkdir pages
+ cd pages
+ git init
+ git commit --allow-empty -m 'initial empty commit'
+ printf "---\nmediawiki:\n  api_url: http://example.com/wiki/api.php\npages:\n  - MainPage\n" > mw2git.yaml
+ git add mw2git.yaml
+ git commit -m 'initial config'
+
+ # Fetch pages
+ mw2git
+
 =head1 DESCRIPTION
 
 This is a workaround for the lack of an "annotate" (aka. "blame")
 feature in the MediaWiki we use locally.
 
-It operates using configuration in and upon a Git repository at the
-current directory.
+It operates using configuration in, and upon a Git repository at, the
+current directory.  There are no options.
 
 
 =head1 CONFIGURATION
 
-By default, it expects F<mw2git.yaml> to exist in the current directory.
+By default, it expects F<mw2git.yaml> to exist in the current
+directory.  Via the (Moose-y) OO interface, it can take configuration
+from elsewhere.
 
 This should contain one hash (dictionary), whose entries are used to
 configure parts of this package.
 
-This configuration may be extended and (atomically) replaced as
-fetching progresses.
+=head2 Keys used
+
+=over 4
+
+=item mediawiki
+
+This is passed to L<MediaWiki::API/new> and should be another hash.
+
+One entry for C<api_url> should be enough.  This is probably
+constructed by replacing the C<index.php> in
+"http://...server.../wiki/index.php" with C<api.php> .
+
+See L<MediaWiki::API> for other options such as proxy control.
+
+=item rvlimit
+
+The MediaWiki C<api.php> limits the maximum number of revisions that
+can be fetched in one query.  This is likely to be 50, 500 or 5000
+depending on context.  This module uses 500 as the default.
+
+=item pages
+
+This list defines the pages to be fetched.
+
+TODO: we could populate 'pages' from a category list at the start of each run.
+
+=item dns_qual
+
+(Optional, for local use only.)  This key is appended to unqualified
+hostnames in F<~/.ssh/ssh-config.yaml>, if you have that file.
+
+=item _page_revs
+
+For internal use.  This hash of pagename to last fetched revision id
+is used to avoid querying the api for previous page revisions.
+
+It is the main reason why the configuration file must be rewritten and
+committed along with the copies of the pages.
+
+=back
+
+=head2 Updating
+
+The configuration is extended in-memory, (atomically) replaced on
+disk, and committed as fetching progresses.
 
 XXX: errors during a run can leave the config out of sync with the committed pages
 so page revisions may get committed again.  One solution would be to
 C<reset --hard> to the last config save commit.  This could be
 automated, at some cost to the principle of least surprise.
-
 
 =cut
 
@@ -88,13 +146,12 @@ sub config_save {
 }
 
 
-=head1 MEDIAWIKI INTERFACE
+=head1 OTHER COMPONENTS
 
-The config key C<mediawiki> should contain a hash suitable for passing
-to L<MediaWiki::API/new>.
+=head2 MediaWiki interface
 
-One entry for C<api_url>, constructed by replacing the C<index.php> in
-"http://...server.../wiki/index.php" with C<api.php> should be enough.
+This is used read-only and anonymously (assumes it does not need to
+log in).
 
 =cut
 
@@ -151,16 +208,18 @@ sub go {
 }
 
 
-=head1 GIT INTERFACE
+=head2 Git interface
 
 Uses L<Git::Repository> to drive Git upon the current directory.
+There is no configuration.
 
 It is assumed that the previous requirement for the existence of the
 configuration file is enough of a sanity check, to prevent messing
 with any other Git repositories' history.
 
-You need to initialise the empty repository directory with a suitable
-configuration file.
+It currently performs only C<git add> and C<git commit> operations,
+but might want to C<git reset --hard> later.  This should probably
+require permission from the configuration.
 
 =cut
 
@@ -172,12 +231,18 @@ sub _build_git {
 }
 
 
-=head1 PAGE TRACKING
+=head2 Page tracking
 
 Configuration lists the pages to fetch, and the last revision fetched
 per page.
 
-TODO: we could populate 'pages' from a category
+The Git author is constructed from the page information, including
+some post-processing to attempt to improve the usefulness of anonymous
+(IP address logged) edits.
+
+The committer and commit timestamp are left to be picked up from the
+environment.  This means that Git commitids will not be reproducible
+between different runs of this code on the same page revisions.
 
 =cut
 
@@ -245,19 +310,28 @@ sub _save_page {
 }
 
 
-=head1 HOSTNAME LOOKUP
+=head2 Hostname lookup
 
 When users do not log in, we get their IP address.  When this is a web
 proxy, we learn nothing; but in a company it is often a one-user
 desktop machine.
 
-Do some lookup to make the history more useful.
-
-This can take advantage of the local username-to-hostname mapping I
-maintain for ssh aliases, if the configuration file is present.
+We do a reverse lookup in the DNS (IPv4) to get a hostname.  Results
+are cached during the run and errors are written out as warnings.
 
 Beware that looking up historically-recorded IP addresses against the
 current DNS is likely to generate surprises.
+
+=head2 Hostname to user lookup
+
+You may safely ignore this part of the code.
+
+If the custom username-to-hostname mapping is present, we include in
+the "anonymous" author info the result of a lookup.
+
+This is a mapping I maintain to generate ssh host aliases, to assist
+with internal user support.  The tool using it is small and not (yet)
+published.
 
 =cut
 
@@ -315,3 +389,15 @@ sub _build_host2nick {
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
+
+
+=head1 AUTHOR
+
+Copyright (C) 2011 Genome Research Limited
+
+Author Matthew Astley L<mca@sanger.ac.uk>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
